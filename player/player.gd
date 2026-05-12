37141c44f3ee576eb2dfd3177823e9ae6da29ae4
SarahLBradburn <sarah.bradburn@hp.com>
2026-05-11 22:23:40 -0600

    background and collisions

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
+const ROLL_SPEED = 125
 
 var input_vector = Vector2.ZERO
+var last_input_vector = Vector2.ZERO
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
@@ -10,26 +12,38 @@ var input_vector = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
 	match state:
-		"MoveState":
-			input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		"MoveState": move_state(delta)
+		"AttackState": pass
+		"RollState": roll_state(delta)
 			
-			if input_vector != Vector2.ZERO:
-				#y-axis is counterintuative in godot (and most game engines)
-				#this lets us set up the animations intuatively in the blend 
-				#state and then just inverse the direction calculation
-				var direction_vector: = Vector2(input_vector.x, -input_vector.y)
-				
-				update_blend_positions(direction_vector)
-				
-			if Input.is_action_just_pressed("attack"):
-				playback.travel("AttackState")
+func move_state(delta: float) -> void:
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 			
-			velocity = input_vector * SPEED
-			move_and_slide()
-		"AttackState":
-			pass
+	if input_vector != Vector2.ZERO:
+		last_input_vector = input_vector
+		#y-axis is counterintuative in godot (and most game engines)
+		#this lets us set up the animations intuatively in the blend 
+		#state and then just inverse the direction calculation
+		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+				
+		update_blend_positions(direction_vector)
+				
+		if Input.is_action_just_pressed("attack"):
+			playback.travel("AttackState")
+			
+		if Input.is_action_just_pressed("roll"):
+			playback.travel("RollState")
+			
+		velocity = input_vector * SPEED
+		move_and_slide()
+		
+func roll_state(delta: float) -> void:
+	velocity = last_input_vector * ROLL_SPEED
+	move_and_slide()
+	
 	
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/Standstate/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/AttackState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/RollState/blend_position", direction_vector)
