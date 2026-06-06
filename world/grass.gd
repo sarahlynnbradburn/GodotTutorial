b617c06d1015f2131bffd442fdc46dcbdd46765d
SarahLBradburn <sarah.bradburn@hp.com>
2026-06-06 16:35:19 -0600

    chopping grass

--- world/grass.gd
+++ world/grass.gd
@@ -1,4 +1,6 @@
 extends Node2D
+const GRASS_EFFECT = preload("res://effects/GrassEffect.tscn")
+
 #You get this by holding ctrl while you drag and drop
 @onready var area_2d: Area2D = $Area2D
 
@@ -17,5 +19,8 @@ func _process(delta: float) -> void:
 
 #Signals should go up the tree, this one comes up from the area2D
 func _on_area_2d_area_entered(other_intruding_area: Area2D) -> void:
+	var grass_effect_instance = GRASS_EFFECT.instantiate()
+	get_tree().current_scene.add_child(grass_effect_instance)
+	grass_effect_instance.global_position = global_position
 	queue_free()
-	print("grass area entered")
+	#print("grass area entered")
