7a56052238f310762ca3ebf68cb7fe070eeabadd
SarahLBradburn <sarah.bradburn@hp.com>
2026-06-08 22:22:21 -0600

    Bat line of sight

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -7,6 +7,7 @@ const SPEED = 30
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 #in the world, you can add the player to individual bats 
 @export var player: Player
@@ -19,8 +20,12 @@ func _physics_process(delta: float) -> void:
 			var player = get_player()
 			if player is Player:
 				velocity = global_position.direction_to(player.global_position) * SPEED
+				
+				#Sets the direction of the sprite, sign gets any number and converts it to -1 or 1
+				sprite_2d.scale.x = sign(velocity.x)
 			else:
 				velocity = Vector2.ZERO
+		
 			move_and_slide()
 
 
@@ -39,5 +44,11 @@ func is_player_in_range() -> bool:
 			result = true
 	return result
 		
-		
+func can_see_player() -> bool:
+	if not is_player_in_range(): return false
+	var player: = get_player()
+	#global position of the player - global position of the bat to get relative position
+	ray_cast_2d.target_position = player.global_position - global_position
+	var has_line_of_sight_to_player: = not ray_cast_2d.is_colliding()
+	return has_line_of_sight_to_player
 		
